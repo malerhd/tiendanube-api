763bb39cbb2cb5ae7db182f708388fe38c1e56f0
malerhd <malena@redhookdata.com>
2025-07-28 14:55:55 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ export default async function handler(req, res) {
   };
 
   try {
-    const response = await fetch("https://www.tiendanube.com/apps/authorize/token", {
+    const response = await fetch("https://www.tiendanube.com/apps/token", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(payload)
